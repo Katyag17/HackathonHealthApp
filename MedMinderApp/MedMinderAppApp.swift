61b81b3d247e9562442e582f6356d4c62fc00a94
NicolasK0 <nkous05@gmail.com>
2025-04-05 12:59:50 -0400

    Initial Commit

---
+++ MedMinderApp/MedMinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedMinderAppApp.swift
+//  MedMinderApp
+//
+//  Created by Nicolas Kousoulas on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MedMinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
